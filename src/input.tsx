48355047cc2493cb88bec3f66267b86f5182c865
Josue JURE <57095041+josueJURE@users.noreply.github.com>
2024-05-22 10:23:04 +0100

    (feat:JS) handle the visibility of the text input field based on the state of the checkbox

--- src/input.tsx
+++ src/input.tsx
@@ -1,24 +1,23 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
 
 interface IProps {
-    id: string | undefined;
-    name: string | undefined;
-
+  id: string | undefined;
+  name: string | undefined;
 }
 
 const CheckboxElements: React.FC<IProps> = (props) => {
-    const [isChecked, setisChecked] = useState(false);
+  const [isChecked, setisChecked] = useState(false);
 
-    const handleChekboxChange = (event: { target: { checked: boolean | ((prevState: boolean) => boolean); }; }) => {
-        setisChecked(event.target.checked);
-        console.log(event.target.checked)
-    }
-   
+  const handleChekboxChange = (event: {
+    target: { checked: boolean | ((prevState: boolean) => boolean) };
+  }) => {
+    setisChecked(event.target.checked);
+    console.log(event.target.checked);
+  };
 
-    return (
-  
-        <input
+  return (
+    <>
+      <input
         type="checkbox"
         id={props.id}
         className="dietary-requirements"
@@ -26,12 +25,19 @@ const CheckboxElements: React.FC<IProps> = (props) => {
         checked={isChecked}
         onChange={handleChekboxChange}
       />
+      {props.name === "has_other_dietary_requirements" && (
+        <input
+          id="user-text"
+          type="text"
+          className={`dietary-requirements ${isChecked ? "on" : "off"}`}
+          value=""
+          name="what_are_user_other_dietary_requirements"
+          placeholder="I don't eat:"
+          disabled={!isChecked}
+        />
+      )}
+    </>
+  );
+};
 
-    
-   
-    )
-
-}
-
-
-export default CheckboxElements
+export default CheckboxElements;
